9641fa97000a188012d3b71deb469b8d34325664
AmarisDavis <112020719+AmarisDavis@users.noreply.github.com>
2022-08-24 16:27:08 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*************************************************************
+*Hello.java
+**************************************************************
+*Amaris Davis
+*08/24/2004
+*CMSC-255-004
+**************************************************************/
+
+public class Hello { 
+	public static void main(String[] args) {
+	
+		System.out.println("Amaris Davis");
+		System.out.println("Hello java");
+		System.out.println(" This program will produce the word hello");
+		System.out.println("08/24/2022");
+		System.out.println("CMSC-255-004");
+		System.out.println();
+	}
+} 
